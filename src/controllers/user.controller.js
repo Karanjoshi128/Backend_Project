cb73e7c4d5326e66b2bf216909b6016c0ea991e1
Karan Joshi <joshikaran.aad.0007@gmail.com>
2024-04-12 14:20:50 +0530

    Added usercontrollers and some minor bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,68 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
-const registerUser = asyncHandler( async (req,res) => {
-    console.log("hemlo");
-    res.status(200).json({
-        message: "ok"
-    })
-})
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, username, password } = req.body;
+  console.log("email :", email);
+
+  if (
+    [fullName, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar image is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar image is required");
+  }
+
+  const user = await User.create({
+    fullName,
+    username : username.toLowerCase(),
+    email,
+    avatar : avatar.url,
+    coverImage : coverImage?.url || "",
+    password
+  })
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  )
+
+  if(!createdUser){
+    throw new ApiError(500 , "Something went wrong while registering the user");
+  }
+
+  return res.status(201).json(
+    new ApiResponse(200 , createdUser , "user registered successfully");
+  ) 
 
 
-export {registerUser}
+
+});
+
+//firstly take the essential credentials from the user(req.body)
+//send those credentials to the db according to the model used
+//registered message as an response
+
+export { registerUser };
